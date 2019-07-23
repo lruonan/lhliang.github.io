de6fb0a3dc462d5eccb1d092489fa1af009ab8df
hongliang liu <570137843@qq.com>
2019-07-23 17:11:11 +0800

    Delete app.js

--- assets/app/js/app.js
+++
@@ -1,7 +0,0 @@
-// change href target property
-jQuery(document).ready(function() {
-	// add target="_blank"
-	jQuery('a[href^="http"]').each(function() {
-		jQuery(this).attr('target', '_blank');
-	});
-});
